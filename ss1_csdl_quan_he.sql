fa3e991d6dece70e08bb828b090408b631b243ca
TungKunz <viettung0203@gmail.com>
2025-11-03 08:31:24 +0700

    ss1

---
+++ ss1_csdl_quan_he.sql
@@ -0,0 +1,24 @@
+create database bai_tap;
+
+use bai_tap;	
+
+-- bài  thực hành 
+create table Student(
+ id int,
+ name varchar(200),
+ age int,
+ country varchar(50)
+);
+-- bài tập
+
+create table Class (
+id int,
+name varchar(200)
+);
+
+create table Teacher (
+id int,
+name varchar(200),
+age int ,
+country varchar(200)
+);
